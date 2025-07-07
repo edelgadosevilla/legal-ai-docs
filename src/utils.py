52db7b9ebc9f781dc1fa79a2f4f2c62abd8c6f6e
edelgadosevilla <98939686+edelgadosevilla@users.noreply.github.com>
2025-07-07 02:58:11 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
+import PyPDF2
 
+def pdf_to_text(pdf_path):
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
